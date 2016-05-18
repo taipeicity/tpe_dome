18a1d0203ee8a4a450725325e0edd3578d3299b0
yungshin <yungshin.chien@gmail.com>
2016-05-18 23:59:57 +0800

    add data

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function(Handlebars, jQuery) {
+  var $ = jQuery, Handlebars = Handlebars;
+
+  //var template = $('#template').html();
+  //var templateScript = Handlebars.compile(template);
+
+  //Handlebars.registerPartial('footer_partial', footer_str);
+  //var data = { "test": "test template" };
+  //var html = templateScript();
+  //$(document.body).html(html);
+})(Handlebars, jQuery)
