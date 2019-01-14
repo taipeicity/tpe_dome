ea78f696914c1ae0857e6c9ab203f6d4b0eb5865
tpe-doit <tpe-doit@users.noreply.github.com>
2019-01-15 01:07:39 +0800

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,6 +1,6 @@
 (function(jQuery) {
     var $ = jQuery;
-    var url = 'https://doit.blob.core.windows.net/portal/portal_NewsList_Dome.json';
+    var url = 'https://www.gov.taipei/OpenData.aspx?SN=BD297A1729022566';
 
     $.ajax({
         url: url,
@@ -15,12 +15,13 @@
 
         for (var i = 0; i < news_data.length; i++) {
             var news_title = news_data[i].title;
-            var news_url = news_data[i].url;
-            var date_str = news_data[i].postdate;
-            var news_deptName = news_data[i].deptName;
+            var news_url = news_data[i].Source;
+            var date_str = news_data[i].發布日期;
+            var news_deptName = news_data[i].發布單位;
 
-            var tmp = date_str.match(/^(\d+)-(\d+)-(\d+)/);
-            var news_date = tmp[1] + '/' + tmp[2] + '/' + tmp[3];
+            //var tmp = date_str.match(/^(\d+)-(\d+)-(\d+)/);
+            //var news_date = tmp[1] + '/' + tmp[2] + '/' + tmp[3];
+            var news_date = date_str.substring(0, 9);
 
             var item_str = '<tr><td>' + (i + 1) + '</td><td><a href="' + news_url + '" target="_blank">' + news_title + '</a></td><td>' + news_deptName + '</td><td>' + news_date + '</td></tr>';
             $('#news_table > tbody').append(item_str);
